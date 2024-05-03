fde182cc9fc7cf6f8e0acaefb2a202ceec7eafa6
abupraise <praiseabu2000@gmail.com>
2024-05-03 07:20:47 +0100

    Backend Architecture

---
+++ src/main/java/praise/hux/contactapp/ContactappApplication.java
@@ -0,0 +1,13 @@
+package praise.hux.contactapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/praise/hux/contactapp/ContactappApplicationTests.java
@@ -0,0 +1,13 @@
+package praise.hux.contactapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
