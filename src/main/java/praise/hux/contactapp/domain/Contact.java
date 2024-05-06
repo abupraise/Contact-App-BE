268a5fee923e73e30294193e9f072b4668a85cdb
abupraise <praiseabu2000@gmail.com>
2024-05-06 07:44:59 +0100

    Updating Mapping

--- src/main/java/praise/hux/contactapp/domain/Contact.java
+++ src/main/java/praise/hux/contactapp/domain/Contact.java
@@ -1,14 +1,15 @@
 package praise.hux.contactapp.domain;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.UuidGenerator;
-
-import java.util.List;
 
 import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_DEFAULT;
 
@@ -31,6 +32,4 @@ public class Contact {
     private String address;
     private String status;
     private String photoUrl;
-    @ManyToOne
-    private User user;
 }
--- src/main/java/praise/hux/contactapp/domain/User.java
+++ src/main/java/praise/hux/contactapp/domain/User.java
@@ -31,9 +31,6 @@ public class User implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Role role;
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
-    private List<Contact> contact;
-
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority(role.name()));
